0cb90544f7d9c1b109db800c7f8fb8339693f8dd
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-02-16 18:19:22 -0500

    Update Queue_6_4.java

--- Chapter 6 - Queues/6.4/Queue_6_4.java
+++ Chapter 6 - Queues/6.4/Queue_6_4.java
@@ -1,35 +1,27 @@
-package queue6_3;
+package queue6_4;
 
 import java.util.Queue;
-import java.util.Deque;
 import java.util.ArrayDeque;
+import java.util.Deque;
 
-public class Queue6_3 {
-		public static void main(String[] args) {
-			Queue<String> queue = new ArrayDeque<String>();
-			queue.add("Bill");
-			queue.add("Lisa");
-			queue.add("Sally");
-			queue.add("Ken");
-			reversed(queue);
+public class Queue6_4 {
+	public static void main(String[] args) {
+		Queue<Integer> queue = new ArrayDeque<>();
+		for(int i = 1; i <= 4; i++) {
+			queue.add(i);
 		}
+		reverse(queue);
 		
-		/* reversed: Returns a new queue 
-		 * Contains the same elements as the given queue but in reverse order
-		 * Leaves given queue in its original state
-		 */
-		public static <E> Queue<E> reversed(Queue<E> queue){
-			Deque<String> deque = new ArrayDeque<>();
-			Queue<String> newQueue = new ArrayDeque<>();
-			
-			// Remove element, push to stack, add back to bottom of queue
-			for(int i = 0; i < queue.size(); i++) {
-				deque.push((String)queue.element());
-				queue.add(queue.remove());
-			}
-			System.out.println("Original - " + queue);
-			System.out.printf("New - " + deque);
-		return (Queue<E>)deque;
+	}
+	
+	/* reverse: Reverse the elements in the specified queue */
+	public static <E> void reverse(Queue<E> queue) {
+		Deque<Integer> deque = new ArrayDeque<Integer>();
+		
+		while(!queue.isEmpty()) {
+			deque.push((Integer)queue.remove());
 		}
+		System.out.println(deque);
+	}
 
 }
