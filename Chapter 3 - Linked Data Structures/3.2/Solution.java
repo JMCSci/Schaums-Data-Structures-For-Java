a04eda3a8fa672cae998f5af00e269aec7af0ebf
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-04-04 15:40:04 -0400

    Update Solution.java
    Fixed Node class and added LinkedList class

--- Chapter 3 - Linked Data Structures/3.2/Solution.java
+++ Chapter 3 - Linked Data Structures/3.2/Solution.java
@@ -1,60 +1,62 @@
-/* Chapter 3.2 */
+/* Chapter 3.3 */
 
-package size;
+package sum;
 
 public class Solution {
 	public static void main(String[] args) {
-		Node list = new Node(33);
-		list.insert(55);
-		list.insert(77);
-		list.insert(99);
-		size(list);	
+		LinkedList list = new LinkedList();
+		list.add(25);
+		list.add(45);
+		list.add(65);
+		list.add(85);
+		sum(list);
 	}
 	
-	// size: Returns the number of nodes in the specified list
-	public static int size(Node list) {
-		System.out.println(list.size);
-		return list.size;
+	// sum: Returns the sum of integers in the specified list
+	public static int sum(LinkedList list) {
+		int sum = 0;
+		Node current = list.head;
+		
+		for(Node p = current; p != null; p = p.next) {
+			sum += p.data;
+		}
+		
+		System.out.println(sum);
+		return sum;
 	}
 
 }
 
+// Node class
 class Node {
 	int data;
-	int size;
-	Node next;			// next node
-	Node head;			// pointer
+	Node next;
 	
 	
-	Node (int data) {
+	Node(int data) {
 		this.data = data;
-		size = 1;
 	}
 	
-	void insert(int data) {
-		if(head == null) {
-			Node node = new Node(data);
-			head = node;
-			size++;
+}
+
+// Wrapper class
+class LinkedList {
+	Node head;				// beginning of list
+	Node tail;				// end of list
+	
+	LinkedList() {
+		
+	}
+	
+	void add(int data) {
+		Node node = new Node(data);
+		if(tail == null) {
+			head = tail = node;
 		} else {
-			Node p = head;
-			while(p != null) {
-				if(p.next == null) {
-					p.next = new Node(data);
-					size++;
-					break;
-				}
-				p = p.next;
-
-			}
-			
-			
+			tail.next = node;
+			tail = tail.next;
 		}
-		
+			
 	}
-		
-	int size() {
-		return size;
-	}
-
+	
 }
