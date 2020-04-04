94c3a799069ffd41fabbc782a753afa2ac67313b
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-04-04 15:52:29 -0400

    Update Solution.java
    - Fixed Node class
    - Added LinkedList class

--- Chapter 3 - Linked Data Structures/3.2/Solution.java
+++ Chapter 3 - Linked Data Structures/3.2/Solution.java
@@ -7,7 +7,63 @@ public class Solution {
 		LinkedList list = new LinkedList();
 		list.add(25);
 		list.add(45);
-		list.add(65);
+		list.add(65);/* Chapter 3.2 */
+
+package size;
+
+public class Solution {
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.insert(55);
+		list.insert(77);
+		list.insert(99);
+		size(list);	
+	}
+	
+	// size: Returns the number of nodes in the specified list
+	public static int size(LinkedList list) {
+		System.out.println(list.size);
+		return list.size;
+	}
+
+}
+
+class Node {
+	int data;
+	int size;
+	Node next;			// next node
+	
+	Node (int data) {
+		this.data = data;
+	}
+	
+
+}
+
+class LinkedList {
+	Node head;			// pointer
+	Node tail;
+	int size = 0;
+	
+	LinkedList() {
+		
+		
+	}
+	
+	void insert(int data) {
+		Node node = new Node(data);
+		if(tail == null) {
+			head = tail = node;
+		} else {
+			tail.next = node;
+			tail = node;
+		}
+		size++;	
+	}
+	
+	
+}
+
 		list.add(85);
 		sum(list);
 	}
